e16145c9bb0904bb01624903810e06abc2769b45
Prince Issar <82021741+PrinceIssar@users.noreply.github.com>
2021-07-23 12:22:32 +0200

    added css from material ui

--- src/App.js
+++ src/App.js
@@ -2,8 +2,34 @@ import React, {useState, useEffect} from "react";
 import './App.css';
 import axios from "axios";
 import Recipe from "./Recipe";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2px 4px',
+        display: 'flex',
+        margin: '10px auto',
+        alignItems: 'center',
+        width: 400,
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 10,
+    },
+
+}));
 
 function App() {
+    const classes = useStyles();
   // useEffect need for api
   const APP_ID = '7a2529fc';
   const APP_KEY = '61efe58ac251fd4cb77555211348d646\t';
@@ -32,17 +58,34 @@ function App() {
   }
 
   return (
-      <div className="App">
-    <form onSubmit={updateQuery}>
-      <input type="text" value={search} onChange={updateSearch}/>
-      <button type="submit">Search</button>
-    </form>
+      <div>
+          <Paper onSubmit={updateQuery} component="form" className={classes.root}>
+
+              <InputBase
+                  type="text"
+                  value={search}
+                  onChange={updateSearch}
+                  className={classes.input}
+                  placeholder="Search for Recipe"
+                  inputProps={{ 'aria-label': 'Search for recipe' }}
+              />
+              <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                  <SearchIcon />
+              </IconButton>
+
+          </Paper>
+    {/*<form onSubmit={updateQuery}>*/}
+    {/*  <input type="text" value={search} onChange={updateSearch}/>*/}
+    {/*  <button type="submit">Search</button>*/}
+    {/*</form>*/}
     {recipes.map((recipe) => (
       // will pass all props here title ,cal,img ,ingre in Recipe.js
-      <Recipe title={recipe.recipe.label}
-              calories={recipe.recipe.calories}
-              image={recipe.recipe.image}
-              ingredients={recipe.recipe.ingredients}
+      <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
       />
     ))}
   </div>
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,17 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 325,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+
+}));
+
+
 
 const Recipe = ({title, calories , image, ingredients}) => {
-    return (<div>
-            <p>{title}</p>
-                <p>{calories}</p>
-                <img src= {image} alt=""/>
-            <ol>
-                {ingredients.map((ingre)=>( // ingredients are in array , so we need to map them
-                    <li>{ingre.text}</li> // .text for the ingredient is in text
-                ))}
-            </ol>
-                </div>
-                );
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <div>
+            <Card className={classes.root}>
+                <CardHeader title={title} subheader={calories}/>
+                <CardMedia className={classes.media} image={image} title={title}/>
+                <CardActions disableSpacing>
+
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                    <div>Ingredients</div>
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        {ingredients.map((ingre)=>( // ingredients are in array , so we need to map them
+                            <Typography>{ingre.text}</Typography> // .text for the ingredient is in text
+                        ))}
+
+                    </CardContent>
+                </Collapse>
+            </Card>
+
+        </div>
+    );
 };
 
 export default Recipe;
